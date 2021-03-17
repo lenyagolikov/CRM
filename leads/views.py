4d58329a39127d77b32ac1052f645dcfd7df29f2
Rachik <jayhosee@gmail.com>
2021-03-17 03:47:02 +0300

    Templates and their rendering

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("<h3>Hello, world!</h3>")
+    return render(request, "leads/home_page.html")

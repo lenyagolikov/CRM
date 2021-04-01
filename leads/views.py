6ee6565f182114f11f57c806b8d200b9e1c1c4c3
Rachik <jayhosee@gmail.com>
2021-04-01 19:56:04 +0300

    Change default form with ModelForm

--- leads/views.py
+++ leads/views.py
@@ -6,11 +6,11 @@ from .forms import LeadForm
 
 def lead_list(request):
     leads = Lead.objects.all()
-    
+
     context = {
         'leads': leads
     }
-    
+
     return render(request, 'leads/lead_list.html', context)
 
 
@@ -26,27 +26,16 @@ def lead_detail(request, pk):
 
 def lead_create(request):
     form = LeadForm()
-    
-    if request.method=='POST':
+
+    if request.method == 'POST':
         form = LeadForm(request.POST)
 
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
-            
+            form.save()
             return redirect('/leads')
-    
+
     context = {
         'form': form
     }
-    
+
     return render(request, 'leads/lead_create.html', context)

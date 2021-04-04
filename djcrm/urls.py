f22e92184b41b67318921a9b3d8bb6bd37798b12
Rachik <jayhosee@gmail.com>
2021-04-04 19:23:52 +0300

    Created static files

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-from .views import landing_page, LandingPage
+from .views import landing_page, LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LandingPage.as_view(), name='landing-page'),
+    path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- djcrm/views.py
+++ djcrm/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 
-class LandingPage(generic.TemplateView):
+class LandingPageView(generic.TemplateView):
     template_name = 'landing.html'
 
 

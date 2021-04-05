0b8e7731f4177207d1f9db91e6fe38b4687241cd
Rachik <jayhosee@gmail.com>
2021-04-05 18:28:04 +0300

    Filter agents

--- agents/views.py
+++ agents/views.py
@@ -9,26 +9,29 @@ from .forms import AgentForm
 class AgentListView(LoginRequiredMixin, generic.ListView):
     """View for displaying agents"""
     template_name = 'agents/agent_list.html'
-    queryset = Agent.objects.all()
     context_object_name = 'agents'
+
+    def get_queryset(self):
+        """Filter agents, hides foreign agents"""
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 
 class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     """View for displaying an agent"""
     template_name = 'agents/agent_detail.html'
-    queryset = Agent.objects.all()
-    context_object_name = 'agent'
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        """Filter agents, hides foreign agents"""
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     """View for creating a new agent"""
     template_name = 'agents/agent_create.html'
     form_class = AgentForm
-    context_object_name = 'form'
-
-    def get_success_url(self):
-        """Redirect after successful creation"""
-        return reverse('agents:agent-list')
 
     def form_valid(self, form):
         """Assign organisation for new agent"""
@@ -36,13 +39,22 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent.organisation = self.request.user.userprofile
         agent.save()
         return super(AgentCreateView, self).form_valid(form)
+        
+    def get_success_url(self):
+        """Redirect after successful creation"""
+        return reverse('agents:agent-list')
 
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
     """View for updating an agent"""
     template_name = 'agents/agent_update.html'
-    queryset = Agent.objects.all()
     form_class = AgentForm
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        """Filter agents, hides foreign agents"""
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
     def get_success_url(self):
         """Redirect after successful updation"""
@@ -52,7 +64,12 @@ class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
 class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
     """View for deleting the selected agent"""
     template_name = 'agents/agent_delete.html'
-    queryset = Agent.objects.all()
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        """Filter agents, hides foreign agents"""
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
     def get_success_url(self):
         """Redirect after successful deletion"""
